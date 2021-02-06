5516452086e011aa9404d93fe6681cd53c6c4eab
Fymidc <60707082+Fymidc@users.noreply.github.com>
2021-02-06 15:39:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $(".hamburger").on('click',function(){
    $("#div2").toggleClass("fade");
    $("#div1").toggleClass("cross1");
    $("#div3").toggleClass("cross3");
+   $(".drpmenu").toggleClass("updown")
 })
 
 $(".search-icon").on('click',function(){
@@ -25,3 +26,63 @@ $("#learnmore").on('click',function(){
     $(".landing-page-info").addClass("infoSlide")
     $(".after-learnmore-click").addClass("learn-more-slide")
 })
+
+$("#camera").on('click',function(){
+    $(".camera-slide-up").addClass("slide-up")
+    $("#img2").addClass("img2")
+    $("#img3").addClass("img3")
+    $("#img4").addClass("img4")
+    $("#img5").addClass("img5")
+
+    $("#img1,#img2,#img3,#img4,#img5").css({"width":"30%","top":"10%","left":"5%"})
+    $(".camera-section-header").addClass("cshslide")
+    $(".camera-section-info").addClass("csislide")
+    $(".camera-section-links").addClass("cslslide")
+    $(".camera-section-button").addClass("csbslide")
+})
+
+$("#appleimg").click(function() {
+    $("#firstpage").css({"z-index":"6"})
+    $('html, body').animate({
+      scrollTop: $("#firstpage").offset().top
+    }, 1000);
+    
+
+  });
+
+
+  $("#battery").on('click',function(){
+    $(".battery-slide-up").addClass("bslide-up")
+
+    $({countNum: $("#counter").text()}).animate({countNum: 30}, {
+        duration: 2000,
+        easing:'linear',
+        step: function() {
+          // What todo on every count
+          $("#counter").text(Math.floor(this.countNum));
+          
+        },
+        complete: function() {
+            $("#counter").text(this.countNum)
+          
+        }
+      });
+
+      $({countNum: $("#counter2").text()}).animate({countNum: 100}, {
+        duration: 2000,
+        easing:'linear',
+        step: function() {
+          // What todo on every count
+          $("#counter2").text(Math.floor(this.countNum));
+          
+        },
+        complete: function() {
+            $("#counter2").text(this.countNum)
+          
+        }
+      });
+      $(".battery-full").addClass("dol")
+      $("#yatayimg").addClass("opacityy")
+  })
+
+
